51f94eb94dc9c2df032468160eed43988edc8b4c
angeelo <artisigngineer@hotmail.com>
2021-03-14 00:33:07 +0800

    新建文件夹

--- fontend/src/router/index.js
+++ fontend/src/router/index.js
@@ -14,6 +14,36 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/uer',
+    name: 'User',
+    component: () => import('../views/User.vue')
+  },
+  {
+    path: '/proposal/register',
+    name: 'Register',
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/proposal/:pid',
+    name: 'Proposal',
+    component: () => import('../views/Proposal.vue')
+  },
+  {
+    path: '/proposal/:pid/tally',
+    name: 'Tally',
+    component: () => import('../views/Tally.vue')
+  },
+  {
+    path: '/vote/:vid',
+    name: 'Vote',
+    component: () => import('../views/Vote.vue')
   }
 ]
 
